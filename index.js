c071582ebb281b6424812188138d6e7324ff0088
max behling <elmachomax123@gmail.com>
2018-03-12 01:51:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector) {
+  document.getElementById('selector')
+  return nestedTarget
+}
+
+function nestedTarget() {
+ 
+}
+
+function increaseRankBy(n) {
+  n = (rankedlist + 4)
+}
+
+function deepestChild() {
+  querySelector(div.grand-node)
+} 
+
